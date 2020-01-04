8fedfbedd3d989edbcfce64447ebe76fe18632fe
Michał Rajek <mrajek9@gmail.com>
2020-01-04 11:18:43 +0100

    Przygotowanie potrzebnych zależności

--- Smaczne.ukw.edu.pl/Startup.cs
+++ Smaczne.ukw.edu.pl/Startup.cs
@@ -15,20 +15,46 @@ namespace Smaczne.ukw.edu.pl
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+            //    Configuration["Data:SportStoreProducts:ConnectionString"]));
+            //services.AddTransient<IProductRepository, EFProductRepository>();
+            //services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
+            //services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            //services.AddTransient<IOrderRepository, EFOrderRepository>();
+            //services.AddTransient<UserRepository>();
+            services.AddMvc();
+            services.AddMemoryCache();
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseSession();
+            app.UseMvc(routes => {
+                routes.MapRoute(
+                    name: null,
+                    template: "{category}/Page{productPage:int}",
+                    defaults: new { controller = "Product", action = "List" });
+                routes.MapRoute(
+                    name: null,
+                    template: "Page{productPage:int}",
+                    defaults: new { controller = "Product", action = "List", productPage = 1 });
+                routes.MapRoute(
+                    name: null,
+                    template: "{category}",
+                    defaults: new { controller = "Product", action = "List", productPage = 1 });
+                routes.MapRoute(
+                    name: null,
+                    template: "",
+                    defaults: new { controller = "Product", action = "List", productPage = 1 });
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(name: null, template: "{controller}/{action}/{id?}");
             });
+            //SeedData.EnsurePopulated(app);
         }
     }
 }
