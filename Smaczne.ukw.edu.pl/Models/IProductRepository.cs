2a3154281252af32a1a34a370d506fb4d460ab83
Michał Rajek <mrajek9@gmail.com>
2020-01-04 12:12:19 +0100

    Utworzenie modelu Product

---
+++ Smaczne.ukw.edu.pl/Models/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Smaczne.ukw.edu.pl.Models
+{
+    public class IProductRepository
+    {
+        IQueryable<Product> Products { get; }
+        void SaveProduct(Product product);
+
+        Product DeleteProduct(int productID);
+    }
+}
