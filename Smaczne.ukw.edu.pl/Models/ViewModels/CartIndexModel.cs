98dde6bb0613b223201b261320cf11ff74b9270a
uuukaaasz <uuukaaasz@gmail.com>
2020-01-04 12:52:46 +0100

    Koszyk

---
+++ Smaczne.ukw.edu.pl/Models/ViewModels/CartIndexModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Smaczne.ukw.edu.pl.Models.ViewModels
+{
+    public class CartIndexModel
+    {
+        public Cart Cart { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
